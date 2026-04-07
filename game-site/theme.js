3348efbcbaec6356a198bfe635cf528dc49ad962
MasonP25 <MasonP25@users.noreply.github.com>
2026-04-06 20:33:04 -0400

    Use Iconify Material Symbols lock for favicon

--- game-site/theme.js
+++ game-site/theme.js
@@ -1716,7 +1716,7 @@
 
 (function() {
   var _z = null, _q = '', _qf = '';
-  var _icn = 'https://icons.duckduckgo.com/ip3/' + String.fromCharCode(103,111,103,117,97,114,100,105,97,110,46,99,111,109) + '.ico';
+  var _icn = 'https://api.iconify.design/material-symbols:lock.svg?color=%23546e7a';
   function _show() {
     if (_z) return;
     _q = document.title;
